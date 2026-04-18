e9660e11bed6046736b5bb22becec1141f8c1f7a
9hannahnine-jpg <9hannahnine@gmail.com>
2026-04-18 10:43:13 -0500

    cleanup: remove arc_sentry_v2 folder

--- arc_sentry_v2/__init__.py
+++
@@ -1,6 +0,0 @@
-from arc_sentry_v2.core.pipeline import ArcSentryV3, ArcSentryV2
-from arc_sentry_v2.models.base_adapter import BaseModelAdapter
-from arc_sentry_v2.models.mistral_adapter import MistralAdapter
-
-__version__ = "3.0.0"
-__all__ = ["ArcSentryV3", "ArcSentryV2", "BaseModelAdapter", "MistralAdapter"]
